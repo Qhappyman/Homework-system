5fa9ff785f8a05d842f580ec18dfa5a587103e3f
EsperanceW <572934280@qq.com>
2019-11-14 19:37:27 +0800

    feat:stu-homework-list

---
+++ Homework-system/src/pages/class-detail/components/body.vue
@@ -0,0 +1,17 @@
+<template>
+  <StuHomeworkList></StuHomeworkList>
+</template>
+
+<script>
+import StuHomeworkList from '../../stu-class-detail/stu-homework/stu-homework-list'
+export default {
+  name: 'Body',
+  components: {
+    StuHomeworkList
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Homework-system/src/pages/stu-class-detail/stu-homework/stu-homework-list.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="container">
+    <div class="title" @click="lookUp">作业题目</div>
+    <div class="content">哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈</div>
+    <div class="whole-text" @click="lookUp">查看全文></div>
+    <div class="deadline">截止日期</div>
+    <el-button @click="lookUp">{{score}}</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StuHomeworkList',
+  data() {
+    return {
+      score: '成绩未公布'
+    }
+  },
+  methods: {
+    lookUp: function() {
+      this.$router.push({path: '/stu-class-detail'})
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .container {
+    position: relative;
+    left: 10%;
+    margin-top: 20px;
+    padding-left: 2%;
+    width: 78%;
+    height: 200px;
+    border: 1px solid #BABCBE;
+    border-radius: 8px;
+  }
+
+  .title {
+    position: relative;
+    top: 10%;
+    width: 85%;
+    height: 15%;
+    font-size: 20px;
+    cursor: pointer;
+  }
+
+  .title:hover {
+    color: #0390FA;
+  }
+
+  .content {
+    position: relative;
+    top: 25%;
+    width: 85%;
+    height: 15%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space:nowrap;
+  }
+
+  .whole-text {
+    position: relative;
+    top: 25%;
+    width: 7%;
+    height: 15%;
+    color: #0390FA;
+    cursor: pointer;
+  }
+
+  .deadline {
+    position: relative;
+    top: 30%;
+    width: 85%;
+    height: 10%;
+    font-size: 15px;
+    color: #BABCBE;
+  }
+
+  .el-button {
+    position: relative;
+    left: 88%;
+  }
+</style>
