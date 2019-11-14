b5803167a8483697da10ca49b87412fc31ec014d
EsperanceW <572934280@qq.com>
2019-11-14 20:56:33 +0800

    feat:stu-notice

---
+++ Homework-system/src/pages/class-detail/components/body.vue
@@ -0,0 +1,17 @@
+<template>
+  <StuNotice></StuNotice>
+</template>
+
+<script>
+import StuNotice from '../../stu-class-detail/stu-notice.vue/stu-notice'
+export default {
+  name: 'Body',
+  components: {
+    StuNotice
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ Homework-system/src/pages/stu-class-detail/stu-notice.vue/stu-notice.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+    <div class="title">公告题目</div>
+    <div class="publish-time">发布时间</div>
+    <div class="content">公告内容</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StuNotice'
+}
+</script>
+
+<style scoped>
+  .container {
+    position: relative;
+    left: 10%;
+    margin-top: 25px;
+    padding-left: 3%;
+    width: 77%;
+    height: 250px;
+    border: 1px solid #BABCBE;
+    border-radius: 8px;
+  }
+
+  .title {
+    position: relative;
+    top: 10%;
+    width: 96%;
+    height: 12%;
+    font-size: 20px;
+  }
+
+  .publish-time {
+    position: relative;
+    top: 15%;
+    width: 96%;
+    height: 8%;
+    font-size: 13px;
+    color: #A0A0A0;
+  }
+
+  .content {
+    position: relative;
+    top: 21%;
+    width: 96%;
+    height: 45%;
+    font-size: 16px;
+  }
+</style>
