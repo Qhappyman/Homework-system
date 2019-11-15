ed2e85816b71885cdb069fd7133b6f15b05175f0
EsperanceW <572934280@qq.com>
2019-11-15 22:04:03 +0800

    register

--- Homework-system/src/pages/register/register.vue
+++ Homework-system/src/pages/register/register.vue
@@ -1,15 +1,47 @@
 <template>
-  <div>
-      register
+<div class="background">
+  <div class="container">
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="活动名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+    </el-form>
   </div>
+</div>
 </template>
 
 <script>
 export default {
-name:'Register'
+  name:'Register',
+   data() {
+      return {
+        form: {
+          name: ''
+        }
+      }
+    }
 }
 </script>
 
-<style>
+<style scoped>
+  body {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
 
+  .background {
+    width: 100%;
+    height: 100%;
+    background-color: red;
+  }
+
+  .container {
+    width: 40%;
+    height: 80px;
+  }
+
+  .el-input {
+    width: 80px;
+  }
 </style>
